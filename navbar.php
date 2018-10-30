5a9d46bf57e5f27d6dce754614ac6c677399a651
fadhlihh <fadhlimorth5@gmail.com>
2018-10-30 22:34:24 +0700

    contact,dropdown category, and product updated

--- navbar.php
+++ navbar.php
@@ -9,12 +9,12 @@
 		      		<i class="fa fa-caret-down"></i></span>
 		    	</button>
 		    	<div class="dropdown-content">
-			      	<a href="#">Link 1</a>
-			     	<a href="#">Link 2</a>
-			      	<a href="#">Link 3</a>
+			      	<a href="#">Clothing</a>
+			     	<a href="#">Electronic</a>
+			      	<a href="#">Food</a>
 		    	</div>
 		  	</div>
-		  	<a href="#contact">Contact</a>
+		  	<a href="contact.php">Contact</a>
 		</div>
 		<div class="navbar-right">
 			<ul>
@@ -25,13 +25,15 @@
 	</nav>
 	<div id="modal" class="modal-signin">
 		<div class="modal-signin-content">
-			<span class="close">&times;</span>
+			<span class="close">x</span>
 			<center>
 				<form method="post">
 					<table class="tbl-signin">
+						<tr><td><h1>Sign in</h1></td></tr>
 						<tr><td><input type="text" name="username" placeholder="Username / Email / NPM"></td></tr>
 						<tr><td><input type="Password" name="password" placeholder="Kata Sandi"></td></tr>
 						<tr><td width="100%"><input type="submit" name="submit" value="Sign In"></td></tr>
+						<tr><td style="font-size: 13px; color:rgba(158, 158, 158, 1);">Belum punya akun POM? <span class="daftar">daftar</span></td></tr>
 					</table>
 				</form>
 			</center>
