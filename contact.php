e8a54269e83177c2b212fb251a33e03ce62d67de
Fifoblivion <fifoblivion9910@gmail.com>
2018-10-31 00:17:17 +0700

    updated contact.php

--- contact.php
+++ contact.php
@@ -12,11 +12,55 @@
 			<h1>CONTACT US</h1>
 			<br><br><br>
 			<center>
+				<!---
 				<table class="contact">
 					<tr><td class="fas fa-envelope margin-right bigger-icon"></td><td class="label">padjadjaranonlineshop@gmail.com</td></tr>
-					<tr><td class="fab fa-line margin-right bigger-icon"></td><td class="label">fadhlihibatul/diemasaksya/fifoblivion</td></tr>
-					<tr><td class="fab fa-whatsapp margin-right bigger-icon"></td><td class="label">0895422509529/085793344993/082256711218</td></tr>
-				</table>
+					<tr><td class="fab fa-line margin-right bigger-icon"></td><td class="label">/diemasaksya/fifoblivion</td></tr>
+					<tr><td class="fab fa-whatsapp margin-right bigger-icon"></td><td class="label">/085793344993/082256711218</td></tr>
+				------>
+				<div class="content">
+					<div class="row">
+						<div class="col-sm-4">
+							<a href="#">
+							<div class="panel panel-primary">
+					        <div class="panel-heading"><img src="http://ssl.gstatic.com/accounts/ui/avatar_2x.png" style="width:100%" alt="Image"></div>
+									<div class="panel-body">  <span class="name">Fadhli Hibatul</span><br/>
+										<span class="fab fa-line margin-right bigger-icon" data-toggle="tooltip" title="fadhlihibatul"></span>
+										<span> <span>fadhlihibatul</span><br/></span>
+										<span class="fab fa-whatsapp margin-right bigger-icon" data-toggle="tooltip" title="0895422509529"></span>
+										<span> <span>0895422509529</span><br/></span>
+									</div>
+				      	</div>
+							</a>
+				    </div>
+						<div class="col-sm-4">
+							<a href="#">
+							<div class="panel panel-primary">
+					        <div class="panel-heading"><img src="http://ssl.gstatic.com/accounts/ui/avatar_2x.png" style="width:100%" alt="Image"></div>
+									<div class="panel-body">  <span class="name">Diemas Aksya</span><br/>
+										<span class="fab fa-line margin-right bigger-icon" data-toggle="tooltip" title="fadhlihibatul"></span>
+										<span> <span>diemasaksya</span><br/></span>
+										<span class="fab fa-whatsapp margin-right bigger-icon" data-toggle="tooltip" title="0895422509529"></span>
+										<span> <span>085793344993</span><br/></span>
+									</div>
+				      	</div>
+							</a>
+				    </div>
+						<div class="col-sm-4">
+							<a href="#">
+							<div class="panel panel-primary">
+					        <div class="panel-heading"><img src="http://ssl.gstatic.com/accounts/ui/avatar_2x.png" style="width:100%" alt="Image"></div>
+									<div class="panel-body">  <span class="name">Muhammad Afif</span><br/>
+										<span class="fab fa-line margin-right bigger-icon" data-toggle="tooltip" title="fadhlihibatul"></span>
+										<span> <span>fifoblivion</span><br/></span>
+										<span class="fab fa-whatsapp margin-right bigger-icon" data-toggle="tooltip" title="0895422509529"></span>
+										<span> <span>082256711218</span><br/></span>
+									</div>
+				      	</div>
+							</a>
+				    </div>
+					</div>
+				</div>
 			</center>
 		</div>
 		<?php require("footer.php") ?>
