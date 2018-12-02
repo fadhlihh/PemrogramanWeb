a34fda6fd0a5973deb829e4018adce15ac18577b
Diemas <43594072+Skydia@users.noreply.github.com>
2018-12-02 17:47:27 +0700

    Added required to fill on each form and its specified format

--- application/views/navbar.php
+++ application/views/navbar.php
@@ -34,8 +34,8 @@
 				<form method="post" action="<?php echo base_url();?>index.php/user-signin">
 					<table class="tbl-signin">
 						<tr><td><h1>Sign in</h1></td></tr>
-						<tr><td><input type="text" name="username" placeholder="Username / Email / NPM"></td></tr>
-						<tr><td><input type="Password" name="password" placeholder="Kata Sandi"></td></tr>
+						<tr><td><input type="text" name="username" placeholder="Username / Email / NPM" required></td></tr>
+						<tr><td><input type="Password" name="password" placeholder="Kata Sandi" required></td></tr>
 						<tr><td width="100%"><input type="submit" name="submit" value="Sign In"></td></tr>
 						<tr><td><span class="gagal">Username atau Password salah</span></td></tr>
 						<tr><td style="font-size: 13px; color:rgba(158, 158, 158, 1);">Belum punya akun POM? <span id="daftar">daftar</span></td></tr>
@@ -44,12 +44,12 @@
 				<form method="post" action="<?php echo base_url();?>index.php/user-signup">
 					<table class="tbl-signup">
 						<tr><td><h1>Sign Up</h1></td></tr>
-						<tr><td><input type="text" name="nama_lengkap" placeholder="Nama Lengkap"></td></tr>
-						<tr><td><input type="text" name="npm" placeholder="NPM"></td></tr>
-						<tr><td><input type="text" name="username" placeholder="Username"></td></tr>
-						<tr><td><input type="text" name="email" placeholder="E-Mail" pattern="^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$"></td></tr>
-						<tr><td><input type="Password" name="password" placeholder="Kata Sandi"></td></tr>
-						<tr><td><input type="text" name="nohp" placeholder="No Telp."></td></tr>
+						<tr><td><input type="text" name="nama_lengkap" placeholder="Nama Lengkap" required></td></tr>
+						<tr><td><input type="text" name="npm" placeholder="NPM" pattern = "(?=.*\d).{12}" title="Harus berjumlah 12 angka" required></td></tr>
+						<tr><td><input type="text" name="username" placeholder="Username" pattern = "^[^-\s]{5,}$" title="Harus berjumlah 5 karakter atau lebih"required></td></tr>
+						<tr><td><input type="email" name="email" placeholder="E-Mail" pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$" title="Harus berupa email. Contoh : emailkamu@unpad.ac.id" required></td></tr>
+						<tr><td><input type="Password" name="password" placeholder="Kata Sandi" pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}" title="Harus mengandung setidaknya satu angka, satu huruf besar, satu huruf kecil, dan setidaknya harus 8 karakter atau lebih" required></td></tr>
+						<tr><td><input type="text" name="nohp" placeholder="No Telp." pattern = "(?=.*\d).{10,}" title="Nomor HP harus berjumlah setidaknya 10 angka" required></td></tr>
 						<tr><td width="100%"><input type="submit" name="submit" value="Sign Up"></td></tr>
 						<tr><td style="font-size: 13px; color:rgba(158, 158, 158, 1);">Sudah punya akun POM? <span id="masuk">sign in</span></td></tr>
 					</table>
